ec92f5f6a5ffb07acfe14e59154464d66272edf4
grace-Adiele8 <adielegrace8@gmail.com>
2023-02-08 22:39:07 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,18 @@ function Navbar() {
     <Stack>
       <Link to='/'>
       <img   src={Logo} alt='logo' style={{ width: '48px', height: '48px', margin: '0 20px'}}/>
-      
       </Link>
+      <Stack direction='row'
+      gap= '40px'
+      fontSize='40px'
+      alignItems='flex-end'
+      >
+        <Link to='/' style={{textDecoration:'none', color: '#3a1212', borderBottom: '3px solid #ff2625'}}>Home</Link>
+        <a href='#exercises' style={{textDecoration:'none', color: '#3a1212'}}>Exercises</a>
+      
+       </Stack>
+     
+      
     </Stack>
   )
 }
